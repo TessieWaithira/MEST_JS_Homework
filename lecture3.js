591dee93612eef7e6a223aeff763ba2391578959
TessieWaithira <terrywaithira1@gmail.com>
2016-10-31 11:12:37 +0000

    final commit for lecture3

--- lecture3.js
+++ lecture3.js
@@ -1,3 +1,23 @@
 /**
  * Created by Jerry on 2016-10-22.
  */
+var2 = 7;
+console.log(var2)
+var var2;
+console.log(var3);
+var var3;
+var3 = 7;
+
+var people = {
+	morningGreeting: 'goodMorning!'
+	greeting.function(){
+		console.log(this.morningGreeting);
+	}
+};
+people.greeting();
+
+var speak = people greeting;
+speak();
+
+var speakBind = speak.bind(people);
+speakBind();
